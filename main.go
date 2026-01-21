5887f0460608fb88039071bad5228612e6805785
kd2718 <korydonati@gmail.com>
2026-01-20 20:58:26 -0800

    working postgres

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"crypto/subtle"
 	"encoding/json"
 	"fmt"
@@ -9,7 +10,11 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
+
+//var conn *pgx.Conn
 
 const PORT = "8081"
 const MAXBODY = 1024 * 1024
@@ -34,32 +39,59 @@ func checkAuth(next http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
-func getSensorData(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Post only", http.StatusMethodNotAllowed)
-		return
+func getSensorData(dbpool *pgxpool.Pool) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Post only", http.StatusMethodNotAllowed)
+			return
+		}
+
+		// limit body size so that we don't read 10 GB of garbage data
+		r.Body = http.MaxBytesReader(w, r.Body, MAXBODY)
+		defer r.Body.Close()
+
+		body, err := io.ReadAll(r.Body)
+
+		if err != nil {
+			// TODO fix
+			http.Error(w, "Error reading body", http.StatusInternalServerError)
+			return
+		}
+		if !json.Valid(body) {
+			log.Printf("Got bad json in body: %v\n", string(body))
+			http.Error(w, "Bad JSON in body", http.StatusBadRequest)
+			return
+		}
+
+		log.Printf("New Post: %v\n", string(body))
+		// basic 201 return. doesn't care about body
+
+		query := "INSERT INTO sensor_data (payload) values ($1)"
+		_, err = dbpool.Exec(r.Context(), query, string(body))
+		if err != nil {
+			log.Printf("Insert failed: %v\n", err)
+			http.Error(w, "Error connection to DB", http.StatusInternalServerError)
+			return
+		}
+		log.Println("Row Inserted")
+		w.WriteHeader(http.StatusCreated)
 	}
+}
 
-	// limit body size so that we don't read 10 GB of garbage data
-	r.Body = http.MaxBytesReader(w, r.Body, MAXBODY)
-	defer r.Body.Close()
-
-	body, err := io.ReadAll(r.Body)
+func get_conn() *pgxpool.Pool {
+	dbURL := fmt.Sprintf("postgres://%s:%s@%s:5432/%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_NAME"),
+	)
+	pool, err := pgxpool.New(context.Background(), dbURL)
 
 	if err != nil {
-		// TODO fix
-		http.Error(w, "Error reading body", http.StatusInternalServerError)
-		return
+		log.Fatalf("DB connection error: %v", err)
+		os.Exit(1)
 	}
-	if !json.Valid(body) {
-		log.Printf("Got bad json in body: %v\n", string(body))
-		http.Error(w, "Bad JSON in body", http.StatusBadRequest)
-		return
-	}
-
-	log.Printf("New Post: %v\n", string(body))
-	// basic 201 return. doesn't care about body
-	w.WriteHeader(http.StatusCreated)
+	return pool
 }
 
 func main() {
@@ -73,7 +105,11 @@ func main() {
 	}
 	serverPort := fmt.Sprintf(":%v", portInt)
 	log.Printf("Now serving on: \"%v\"\n", serverPort)
-	http.HandleFunc("/sensor", checkAuth(getSensorData))
+
+	pool := get_conn()
+	defer pool.Close()
+
+	http.HandleFunc("/sensor", checkAuth(getSensorData(pool)))
 
 	log.Fatal(http.ListenAndServe(serverPort, nil))
 }
