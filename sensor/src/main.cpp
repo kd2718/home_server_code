bf93d88088cd43ce788da65c532f5d91a30a2efc
kd2718 <korydonati@gmail.com>
2026-01-26 20:06:30 -0800

    seems ok

--- sensor/src/main.cpp
+++ sensor/src/main.cpp
@@ -3,25 +3,35 @@
 // GPIO 2 is the standard onboard LED for most ESP32 DevKitC boards
 const int ledPin = 2;
 const int a2dPin=33;
-const int poutPin=4;
+const int poutPin=23;
 
 
 void setup()
 {
+    // set bod rate
+    Serial.begin(115200);
+
     // Initialize the digital pin as an output
     pinMode(ledPin, OUTPUT);
-    Serial.begin(115200);
     pinMode(poutPin, OUTPUT);
+
+    // set initial pin output
     digitalWrite(poutPin,HIGH);
     digitalWrite(ledPin, HIGH);
+
 }
 
 void loop()
 {
-    digitalWrite(ledPin, LOW);
+    digitalWrite(poutPin,HIGH);
+    digitalWrite(ledPin, HIGH);
     uint16_t sensorValue = analogRead(a2dPin);
-    delay(1000);
-    Serial.println("Test 123");
-    Serial.printf("sensor reading: %d\n", sensorValue);
+    Serial.printf("sensor reading HIGH: %d\n", sensorValue);
+    delay(5000);
 
+    digitalWrite(poutPin,LOW);
+    digitalWrite(ledPin, LOW);
+    sensorValue = analogRead(a2dPin);
+    Serial.printf("sensor reading LOW: %d\n", sensorValue);
+    delay(5000);
 }
